e78b07a2af48b1acf635f3737cf4972ff010f4ba
Christian <mano.power@outlook.com>
2023-11-19 22:15:42 +0000

    Add project files.

---
+++ src/includes.hpp
@@ -0,0 +1,8 @@
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <iostream>
+#include <tchar.h>
+#include <vector>
+#include <cstdlib>
+
+using std::cout, std::cin, std::endl, std::vector;
---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+﻿#include "includes.hpp"
+#include "externMem.hpp"
+
+int main(int argc, char** argv[])
+{
+    cout << "Continue with program or quit?" << endl << "Y or other: ";
+    char cont; cin >> cont;
+
+    switch (cont)
+    {
+    case 'y':
+    case 'Y':
+    {
+        break;
+    }
+    case 'n':
+    case 'N':
+    {
+        return 0;
+    }
+
+    }
+
+    HWND hWnd = FindWindowA(NULL, "Geometry Dash"); // get handle to gd
+
+    if (hWnd != FALSE);
+    DWORD processID = NULL; //process handle
+    GetWindowThreadProcessId(hWnd, &processID);
+    HANDLE processHandle = NULL;
+    processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processID);
+    if (processHandle == INVALID_HANDLE_VALUE || processHandle == NULL);
+
+    char gameModule1[] = "GeometryDash19.exe";
+    DWORD gameBaseAdd1 = GetModuleBaseAddress(_T(gameModule1), processID); // get module base address
+
+
+    //attempts (only for testing, no real practical use)
+    DWORD attAddress = 0x0016A1A0;
+    vector<DWORD> attOffsets{ 0x144, 0x214, 0xAC, 0x18C, 0xAC, 0xAC, 0x2E8 };
+    DWORD attPtrAddress = GetPointerAddress(hWnd, gameBaseAdd1, attAddress, attOffsets);
+
+    //write memory
+    while (true)
+    {
+        int attempts = 1336;
+        WriteProcessMemory(processHandle, (LPVOID*)(attPtrAddress), &attempts, 4, 0); // '4' for 4 bytes
+    }
+
+    return 0;
+ }
