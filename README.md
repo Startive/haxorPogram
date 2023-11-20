c71fc735a467b14f88e0fd68c2330b8983e1567e
Christian <mano.power@outlook.com>
2023-11-20 17:33:25 +0000

    change some stuff around

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # haxor
 
 pointers is the reason
+
+todo: make it a dll that you attatch to the game
--- src/main.cpp
+++ src/main.cpp
@@ -22,28 +22,56 @@ int main(int argc, char** argv[])
 
     HWND hWnd = FindWindowA(NULL, "Geometry Dash"); // get handle to gd
 
-    if (hWnd != FALSE);
+    if (hWnd == NULL)
+    {
+        cout << "Geometry Dash is not open!" << endl;
+        return 1;
+    }
+
     DWORD processID = NULL; //process handle
     GetWindowThreadProcessId(hWnd, &processID);
     HANDLE processHandle = NULL;
     processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processID);
     if (processHandle == INVALID_HANDLE_VALUE || processHandle == NULL);
 
-    char gameModule1[] = "GeometryDash19.exe";
+    char gameModule1[] = "GeometryDash19.exe"; // change if executable name is different
     DWORD gameBaseAdd1 = GetModuleBaseAddress(_T(gameModule1), processID); // get module base address
 
 
-    //attempts (only for testing, no real practical use)
-    DWORD attAddress = 0x0016A1A0;
-    vector<DWORD> attOffsets{ 0x144, 0x214, 0xAC, 0x18C, 0xAC, 0xAC, 0x2E8 };
-    DWORD attPtrAddress = GetPointerAddress(hWnd, gameBaseAdd1, attAddress, attOffsets);
+    cout << "Spoof attempts (A), or change FPS (F)?: " << endl;
+    char op; cin >> op;
 
-    //write memory
-    while (true)
+    switch (op)
     {
-        int attempts = 1336;
-        WriteProcessMemory(processHandle, (LPVOID*)(attPtrAddress), &attempts, 4, 0); // '4' for 4 bytes
+    case 'A':
+    case 'a':
+    {
+        //attempts (only for testing, no real practical use)
+        DWORD attAddress = 0x0016A1A0;
+        vector<DWORD> attOffsets{ 0x144, 0x214, 0xAC, 0x18C, 0xAC, 0xAC, 0x2E8 }; //all pointer offsets
+        DWORD attPtrAddress = GetPointerAddress(hWnd, gameBaseAdd1, attAddress, attOffsets);
+
+        //write memory
+        while (true)
+        {
+            int attempts = 100;
+            WriteProcessMemory(processHandle, (LPVOID*)(attPtrAddress), &attempts, 4, 0); // '4' for 4 bytes
+        }
+        break;
+    }
+    case 'F':
+    case 'f':
+    {
+        cout << "not implemented yet" << endl;
+        break;
+    }
+    default:
+    {
+        cout << "why" << endl;
     }
 
+    }
+
+    system("pause");
     return 0;
  }
